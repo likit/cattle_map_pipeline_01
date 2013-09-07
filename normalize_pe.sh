2f83a843c4215d52f1ac8189c7c415c12c06c452
Likit Preeyanon <preeyano@msu.edu>
2013-09-07 09:57:27 -0400

    add normalize_pe.sh

---
+++ normalize_pe.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -login
+#PBS -l nodes=1:ppn=1,mem=24gb,walltime=24:00:00
+#PBS -M preeyano@msu.edu
+#PBS -m abe
+#PBS -N Normalize_C20_k20_paired
+
+cd /mnt/lustre_scratch_2012/preeyanon/cattle_map/paired/diginorm
+python ~/khmer/scripts/normalize-by-median.py -p -k 20 -C 20 -N 4 -x 4e9 --savehash normC20k20.kh *.fastq
