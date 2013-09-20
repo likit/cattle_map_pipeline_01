5a0d14abf920df75ff4bc77cd2b4839652bdd9e6
Likit Preeyanon <preeyano@msu.edu>
2013-09-20 00:39:40 -0400

    update path

--- interleave.sh
+++ interleave.sh
@@ -4,11 +4,11 @@
 #PBS -m abe
 #PBS -N Interleave_all_paired_reads
 
-cd /mnt/lustre_scratch_2012/preeyanon/cattle_map/paired/diginorm
-find ../raw/ -name "*pe_trim1.fastq" | while read FILE
+cd /mnt/lustre_scratch_2012/preeyanon/cattle_map/paired/
+find ./raw/ -name "*pe_trim1.fastq" | while read FILE
 do
     r1=$FILE
     r2=$(echo $FILE | sed s/trim1/trim2/)
     out=$(echo $(basename $FILE) | sed s/trim1/trim/)
-    python /mnt/home/preeyano/khmer/scripts/interleave-reads.py $r1 $r2 > $out
+    python ~/khmer/scripts/interleave-reads.py $r1 $r2 > $out
 done
