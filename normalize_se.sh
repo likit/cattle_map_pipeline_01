5cb16f4eaa29590c66840ec572151d42a891770c
Likit Preeyanon <preeyano@msu.edu>
2013-09-20 09:57:12 -0400

    edit filename pattern

--- normalize_se.sh
+++ normalize_se.sh
@@ -4,4 +4,4 @@
 #PBS -N Normalize_C20_k20_paired
 
 cd /mnt/lustre_scratch_2012/preeyanon/cattle_map/paired/raw
-python ~/khmer/scripts/normalize-by-median.py -C 20 --loadhash normC20k20.kh --savehash normC20k20.kh *trim_unpaired.fastq
+python ~/khmer/scripts/normalize-by-median.py -C 20 --loadhash normC20k20.kh --savehash normC20k20.kh *001.trim_unpaired.fastq
