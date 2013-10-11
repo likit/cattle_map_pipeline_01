014162ff47b199af780e13748144051b51d8f2fb
Likit Preeyanon <preeyano@msu.edu>
2013-10-11 17:13:25 -0400

    add scripts to run blast against human and cow proteins

---
+++ run_blast_cow_x_cow.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -login
+#PBS -l nodes=1:ppn=4,mem=24gb,walltime=72:00:00
+#PBS -m abe
+#PBS -N blast_cow_x_cow_proteins
+#PBS -A ged-intel11
+
+cd /mnt/ls12/preeyanon/cattle_map/paired/assembly/trinity_out_dir_stranded_new_partitions
+blastall -i Trinity.fasta.part.renamed.fasta -d cow.protein.faa -e 1e-3 -p blastx -o cow.x.cow -a 8 -v 4 -b 4
---
+++ run_blast_cow_x_human.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -login
+#PBS -l nodes=1:ppn=4,mem=24gb,walltime=72:00:00
+#PBS -m abe
+#PBS -N blast_cow_x_human_proteins
+#PBS -A ged-intel11
+
+cd /mnt/ls12/preeyanon/cattle_map/paired/assembly/trinity_out_dir_stranded_new_partitions
+blastall -i Trinity.fasta.part.renamed.fasta -d human.protein.faa -e 1e-3 -p blastx -o cow.x.human -a 8 -v 4 -b 4
