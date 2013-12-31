28389c857c5508525c8832c3ae12df2d43d935a0
Likit Preeyanon <preeyano@msu.edu>
2013-12-30 22:22:39 -0500

    update normalize_pe.sh

--- normalize_pe.sh
+++ normalize_pe.sh
@@ -3,5 +3,5 @@
 #PBS -m abe
 #PBS -N Normalize_C20_k20_paired
 
-cd /mnt/lustre_scratch_2012/preeyanon/cattle_map/paired/raw
+cd ${PBS_O_WORKDIR}
 python ~/khmer/scripts/normalize-by-median.py -p -k 20 -C 20 -N 4 -x 4e9 --savehash normC20k20.kh *pe_trim.fastq
