ca9a5375048c3a7696c03823eabcdf022f1abe14
Likit Preeyanon <preeyano@msu.edu>
2014-01-04 23:44:40 -0500

    update extract-paired-reads.py

--- extract_paired_reads.sh
+++ extract_paired_reads.sh
@@ -2,11 +2,11 @@
 #PBS -l nodes=1:ppn=1,mem=24gb,walltime=12:00:00
 #PBS -M preeyano@msu.edu
 #PBS -m abe
-#PBS -N extract-paired-reads
+#PBS -N Extract_paired_reads
 #PBS -A ged-intel11
 
 
-cd /mnt/lustre_scratch_2012/preeyanon/cattle_map/paired/diginorm
+cd ${PBS_O_WORKDIR}
 for f in *.pe*abundfilt
 do
     python ~/khmer/scripts/extract-paired-reads.py $f
